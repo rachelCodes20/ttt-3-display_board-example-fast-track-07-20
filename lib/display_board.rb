65ad2b11b8aff99a8c8b65058305a91b1c336af4
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 21:31:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@ def display_board()
   
 #puts "Welcome to Tic Tac Toe"
 puts "   |   |  "
-puts "-----------"
+puts " -----------"
 puts "   |   |  " 
-puts "-----------"
+puts " -----------"
 puts "   |   |  "
 end
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
